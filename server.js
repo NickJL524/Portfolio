0da0b914c6f702db258a4fc75fc0aa3bd5cb47fa
NickJL524 <nicklamothe524@gmail.com>
2021-04-05 20:30:47 -0400

    new

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
+const public = path.resolve("./client/build")
 
 //routing to my public folder
-app.use(express.static("./public/build"));
+app.use(express.static(public));
 
 
 app.get("*", (request, response) => {
-  response.sendFile(path.resolve("./public/index.html"));
+  response.sendFile(path.resolve(public + "/index.html"));
 });
 
 
